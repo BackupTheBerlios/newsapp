e167b7bc7c852bf1ac9f5a48963ca231701195ce
newsletter <newsletter>
2007-02-20 11:10:54 +0000

    *** empty log message ***

---
+++ Newsletter/src/newsletter/app/business/Author.java
@@ -0,0 +1,34 @@
+package newsletter.app.business;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Author {
+	private String name;
+	private String firstName;
+	private int id;
+	private static int actId = 0;
+	private Map<Integer, Article> articleCollection = new HashMap<Integer, Article>();
+	
+	public Author(String name, String firstName){
+		this.name = name;
+		this.firstName = firstName;
+		this.id = createId();
+	}
+	private static synchronized int createId() {
+		return ++actId;
+	}
+	
+	public int getId(){
+		return this.id;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public String getFirstName(){
+		return this.firstName;
+	}
+
+}
