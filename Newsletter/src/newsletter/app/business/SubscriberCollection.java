87987da96c8252fcc97a6efa8e971c30f9da1ccf
newsletter <newsletter>
2007-02-22 22:03:51 +0000

    *** empty log message ***

--- Newsletter/src/newsletter/app/business/SubscriberCollection.java
+++ Newsletter/src/newsletter/app/business/SubscriberCollection.java
@@ -5,12 +5,26 @@ import java.util.Map;
 
 
 
+/**
+ * Contains all registred subscribers of the newsletter-system
+ * @author Schnl1@bfh.ch
+ * @author Zumsr1@bfh.ch
+ *
+ */
 public class SubscriberCollection {
 	private Map<String, Subscriber> subscribers = new HashMap<String, Subscriber>();
 	private static SubscriberCollection instance;
 	
+	/**or is private, because it's a singelton.
+	 * Construct
+	 */
 	private SubscriberCollection(){}
 	
+	
+	/**
+	 * returns always the same instance of SubscriberCollection
+	 * @return SubscriberCollection instance
+	 */
 	public static synchronized SubscriberCollection getInstance(){
 		if (instance == null){
 			instance = new SubscriberCollection();
@@ -18,6 +32,12 @@ public class SubscriberCollection {
 		return instance;
 	}
 	
+	/**
+	 * Creates a new Subscriber if it doesn't already exist.
+	 * @param eMailAddress
+	 * @param password
+	 * @return boolean, could the system create the new subscriber?
+	 */
 	public boolean add(String eMailAddress, String password){
 		if (!exists(eMailAddress)){
 			subscribers.put(eMailAddress, new Subscriber(eMailAddress, password));
@@ -26,14 +46,32 @@ public class SubscriberCollection {
 		return false;
 	}
 	
-	public Subscriber login(String eMailAddress, String password){
-		return null;
+	/**
+	 * perform a login, searchs an user with this eMailAddress an d password.
+	 * @param eMailAddress
+	 * @param password
+	 * @return boolean, are eMailAdress and password correct 
+	 */
+	public boolean login(String eMailAddress, String password){
+		return this.getSubscriber(eMailAddress).checkPassword(password);
 	}
 	
+	
+	/**
+	 * checks if the subscriber with this eMailAddress already exists.
+	 * @param eMailAddress
+	 * @return boolean, exists a subscriber with this eMailAddress?
+	 */
 	public boolean exists(String eMailAddress){
 		return (getSubscriber(eMailAddress) != null);
 	}
 	
+	
+	/**
+	 * gets the subscriber-object from eMailAddress. If no such subscriber exists, the system returns null.
+	 * @param eMailAddress
+	 * @return subscriberObject from eMailAddress
+	 */
 	public Subscriber getSubscriber(String eMailAddress){
 		return subscribers.get(eMailAddress);
 	}
