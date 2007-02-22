fd07248979db7af00ba51f997f524c4bd29142b9
newsletter <newsletter>
2007-02-22 21:54:46 +0000

    *** empty log message ***

--- Newsletter/src/newsletter/app/business/OnlineSubscriberCollection.java
+++ Newsletter/src/newsletter/app/business/OnlineSubscriberCollection.java
@@ -3,13 +3,28 @@ package newsletter.app.business;
 import java.util.HashMap;
 import java.util.Map;
 
-
+/**
+ * @author Zumsr1@bfh.ch
+ * @author Schnl1@bfh.ch
+ * @version 1.0
+ * 
+ * OnlineSubscriberCollection enthält alle Subscriber, die zur Zeit online sind.
+ */
 public class OnlineSubscriberCollection {
 	private static Map<String, Subscriber> onlineSubscribers = new HashMap<String, Subscriber>();
 	private static OnlineSubscriberCollection instance;
 	
+	
+	/**
+	 * Constructor ist privat, weil es ein Singelton ist.
+	 */
 	private OnlineSubscriberCollection(){}
 	
+	
+	/**
+	 * Gibt immer die gleiche Instanz von OnlineSubscriberCollection zurück
+	 * @return OnlineSubscriberCollection
+	 */
 	public static synchronized OnlineSubscriberCollection getInstance(){
 		if (instance == null){
 			instance = new OnlineSubscriberCollection();
@@ -17,16 +32,30 @@ public class OnlineSubscriberCollection {
 		return instance;
 	}
 	
+	/**
+	 * Perform a login. Adds subscriber to onlineSubscriberCollection
+	 * @param subscriber
+	 */
 	public void login(Subscriber subscriber){
 		if (!isAlreadyLoggedIn(subscriber)){
-			onlineSubscribers.get(subscriber);
+			onlineSubscribers.put(subscriber.getEMailAddress(), subscriber);
 		}
 	}
 	
+	/**
+	 * Perform logout. Remove Subscriber from onlineSubscriberCollection
+	 * @param subscriber
+	 */
 	public void logout(Subscriber subscriber){
 		onlineSubscribers.remove(subscriber);
 	}
 	
+	
+	/**
+	 * Checks, if an user is already logged in or not.
+	 * @param subscriber
+	 * @return Boolean, is the user already logged in?
+	 */
 	private boolean isAlreadyLoggedIn(Subscriber subscriber){
 		return onlineSubscribers.containsValue(subscriber);
 	}
