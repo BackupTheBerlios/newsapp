48837852da1ce3a6ac9ecf46aa5b5d062ce5d68f
newsletter <newsletter>
2007-02-23 11:48:29 +0000

    *** empty log message ***

--- Newsletter/src/newsletter/app/business/Article.java
+++ Newsletter/src/newsletter/app/business/Article.java
@@ -3,6 +3,11 @@ package newsletter.app.business;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Article of an issue
+ * @author schnl1@bfh.ch
+ * @author zumsr1@bfh.ch
+ */
 public class Article {
 	private String title;
 	private String text;
@@ -10,6 +15,12 @@ public class Article {
 	private int id;
 	private Map<String, Author> authorCollection = new HashMap<String, Author>();
 
+	/**
+	 * Constructor of an article
+	 * @param id id of the article
+	 * @param title title of the article
+	 * @param text text of the article
+	 */
 	public Article(int id, String title, String text){
 		this.id = id;
 		this.title = title;
@@ -17,38 +28,74 @@ public class Article {
 	}
 	
 
+	/**
+	 * set the issue containing the article
+	 * @param issue issue
+	 */
 	public void setIssue(Issue issue){
 		this.issue = issue;
 	}
 	
+	/**
+	 * set the title of the article
+	 * @param title title of the article
+	 */
 	public void setTitle(String title){
 		this.title = title;
 	}
 	
+	/**
+	 * set the text of the article
+	 * @param text of the article
+	 */
 	public void setText(String text){
 		this.text = text;
 	}
 	
+	/**
+	 * get the id of the article
+	 * @return id
+	 */
 	public int getId(){
 		return this.id;
 	}
 	
+	/**
+	 * get the title of the article
+	 * @return title
+	 */
 	public String getTitle(){
 		return this.title;
 	}
 	
+	/**
+	 * get the text of the article
+	 * @return text
+	 */
 	public String getText(){
 		return this.text;
 	}
 	
+	/**
+	 * get the issue containing the article
+	 * @return issue
+	 */
 	public Issue getIssue(){
 		return this.issue;
 	}
 
+	/**
+	 * add an author to the collection
+	 * @param author author
+	 */
 	public void addAuthor(Author author){
 		authorCollection.put(author.getEMailAddress(), author);
 	}
 	
+	/**
+	 * get all authors that have written the article
+	 * @return authors
+	 */
 	public Map getAuthors(){
 		return this.authorCollection;
 	}
