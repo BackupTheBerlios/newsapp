3991a96107d050a4ea790bc313bb52f09a7c559f
newsletter <newsletter>
2007-12-05 09:33:42 +0000

    *** empty log message ***

---
+++ euro08/src/ch/bfh/euro08/jsf/beans/EmailValidator.java
@@ -0,0 +1,40 @@
+package ch.bfh.euro08.jsf.beans;
+
+import javax.faces.validator.*;
+import javax.faces.context.*;
+import javax.faces.component.*;
+
+import javax.faces.application.*;
+
+
+public class EmailValidator implements Validator {
+  
+  public void validate(FacesContext context, UIComponent
+      component, Object toValidate) {
+    boolean isValid = true;
+    String value = null;
+    
+    if ((context == null) || (component == null)) {
+      throw new NullPointerException();
+    }
+    if (!(component instanceof UIInput)) {
+      return;
+    }
+    if (null == toValidate) {
+      return;
+    }
+    value = toValidate.toString();
+    int atIndex = value.indexOf('@');
+    if (atIndex < 0) {
+      isValid = false;
+    }
+    else if (value.lastIndexOf('.') < atIndex) {
+      isValid = false;
+    }
+    if ( !isValid ) {
+      FacesMessage errMsg = new FacesMessage("errorEmailFormat");
+      FacesContext.getCurrentInstance().addMessage(null, errMsg);
+      throw new ValidatorException(errMsg);
+    }
+  }
+}
