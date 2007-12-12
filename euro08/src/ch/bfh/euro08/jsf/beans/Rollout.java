461278ca8f990f413de8321cafecf62f712d8dc8
newsletter <newsletter>
2007-12-12 08:35:49 +0000

    *** empty log message ***

--- euro08/src/ch/bfh/euro08/jsf/beans/Rollout.java
+++ euro08/src/ch/bfh/euro08/jsf/beans/Rollout.java
@@ -1,77 +1,23 @@
 package ch.bfh.euro08.jsf.beans;
 
-import java.util.Iterator;
 import java.util.List;
-
-
-
 import org.hibernate.Query;
 import org.hibernate.Session;
-
-import ch.bfh.euro08.hibernate.beans.Match;
 import ch.bfh.euro08.hibernate.beans.Ordering;
 import ch.bfh.euro08.hibernate.beans.StadeCategory;
-import ch.bfh.euro08.hibernate.beans.User;
 import ch.bfh.euro08.util.HibernateUtil;
-import ch.bfh.euro08.util.JSFUtil;
-
-
 
 public class Rollout {
 
-
-
 	public Rollout() {
 	}
-	
-	public boolean getNotDone() {
-
-		User managedUserBean = (User) JSFUtil.getManagedObject("user");
-		Query q = null;
-		List<Ordering> order_results = null;
-		int userid = managedUserBean.getId();
-
-		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-		session.beginTransaction();
-
-		// ORDER
-		q = session.createQuery("select from Ordering ord where ord.status = 1");
-		order_results = q.list();
-	
-
-		if (order_results.size() > 0)
-			return false;
-		return true;
-	}
-	
-	public boolean getDone() {
-
-		User managedUserBean = (User) JSFUtil.getManagedObject("user");
-		Query q = null;
-		List<Ordering> order_results = null;
-		int userid = managedUserBean.getId();
-
-		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-		session.beginTransaction();
-
-		// ORDER
-		q = session.createQuery("select from Ordering ord where ord.status = 1");
-		order_results = q.list();
-	
-
-		if (order_results.size() > 0)
-			return true;
-		return false;
-	}
-	
-	public String rollback() {
-		List<Ordering> order = null;
 		
+	public String rollback() {
+		List<Ordering> order = null;	
 		Query q = null;
 
 		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
 		session.beginTransaction();
-
 
 		q = session.createQuery("select from Ordering ord order by ticket_fkey asc");
 		order = q.list();
@@ -87,13 +33,10 @@ public class Rollout {
 	
 	public String rollout() {
 		List<Ordering> order = null;
-		List<StadeCategory> stadion = null;
-		
 		Query q = null;
 
 		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
 		session.beginTransaction();
-
 
 		q = session.createQuery("select from Ordering ord order by ticket_fkey asc");
 		order = q.list();
@@ -104,14 +47,12 @@ public class Rollout {
 		int seatnumber = 1;
 		int temp = 0;
 		for (int i = 0; i < order.size(); i++) {
-			System.out.println("EENNTTEERRIINNGG");
 			
-			// If new order id
+			// If the Sector, Stadion or Game changes
 			if (temp != order.get(i).getTicket_fkey().getId()) {
-				
 				temp = order.get(i).getTicket_fkey().getId();
 				
-				// SEATS 
+				// Ruft die Anzahlt der freien SEATS für den aktuellen Match, Stadion, Kategorie ab
 				List<StadeCategory> stadecategory_result = null;
 				q = session.createQuery("select from StadeCategory std where std.stade_fkey = :instade and std.category_fkey = :incat");
 				q.setParameter("instade", order.get(i).getTicket_fkey().getMatch_fkey().getStade_fkey().getId());
@@ -121,7 +62,7 @@ public class Rollout {
 				available = std.getTickets();
 				seatnumber = 1;
 				
-				// REQUESTED 
+				// Ruft die Anzahl der Tickets ab die REQUESTED wurden
 				List<Ordering> order_result = null;
 				q = session.createQuery("select from Ordering ord where ord.ticket_fkey = :inticket");
 				q.setParameter("inticket", order.get(i).getTicket_fkey().getId());
@@ -132,29 +73,61 @@ public class Rollout {
 				}
 				
 				orig_step = (double)requested / (double)available;
-				
 				System.out.println("Ava: " + available + ", Req: " + requested + ", Step: " + orig_step + ", Temp: " + temp);
 			}
 			
 			if (available > 0 && step <= 1.0) {
-				System.out.println("EENNTTEERRIINNGG 22");
+				// Setze die Sitzplatznummer in die Order, diese Order hat ein Ticket erhalten
 				order.get(i).setSeat(seatnumber);
 				seatnumber++;
 				available--;
-			
+				
+				// Diese Order hat ein Tickets erhalten
 				order.get(i).setStatus(true);
 				
 				session.update(order.get(i));
 				step = orig_step;
-			}else{
+			} else {
 				step--;
 			}
 		}
 		session.getTransaction().commit();
 		
 		return "success";
-	
 	}
 
+	// Methode zum Anzeigen des Rolloutbuttons. Wird über rendered="#{rollout.notDone}" gesteuert
+	public boolean getNotDone() {
+		Query q = null;
+		List<Ordering> order_results = null;
+	
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+
+		// Liest alle Orderings aus, und prüft ob es bereits zugeteilte tickets gibt
+		q = session.createQuery("select from Ordering ord where ord.status = 1");
+		order_results = q.list();
+	
+		if (order_results.size() > 0)
+			return false;
+		return true;
+	}
+	
+	// Methode zum Anzeigen des Rolloutbuttons. Wird über rendered="#{rollout.done}" gesteuert
+	public boolean getDone() {
+		Query q = null;
+		List<Ordering> order_results = null;
+
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+
+		// Liest alle Orderings aus, und prüft ob es bereits zugeteilte tickets gibt
+		q = session.createQuery("select from Ordering ord where ord.status = 1");
+		order_results = q.list();
+	
+		if (order_results.size() > 0)
+			return true;
+		return false;
+	}
 	
 }
